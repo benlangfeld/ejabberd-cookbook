f5d68b00fe37bbddf449076ef0e42b1ca9b2acf3
Ben Langfeld <ben@langfeld.me>
2013-11-08 16:17:32 -0200

    Fill out metadata some more

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,9 @@ description      'Installs/Configures ejabberd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'apt'
+recipe "ejabberd", "Install ejabberd and configure"
+
+depends 'apt', '~> 2.2'
+
+supports "debian", ">= 6.0"
+supports "ubuntu", ">= 10.04"
