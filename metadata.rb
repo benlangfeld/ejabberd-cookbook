dd15b470db1e806687d82f77d5437364a7fc6e85
Ben Langfeld <ben@langfeld.me>
2013-11-08 16:41:14 -0200

    Be consistent about using single quotes in metadata

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,9 @@ description      'Installs/Configures ejabberd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-recipe "ejabberd", "Install ejabberd and configure"
+recipe 'ejabberd', 'Install ejabberd and configure'
 
 depends 'apt', '~> 2.2'
 
-supports "debian", ">= 7.1"
-supports "ubuntu", ">= 10.04"
+supports 'debian', '>= 7.1'
+supports 'ubuntu', '>= 10.04'
